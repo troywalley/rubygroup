b7bb0b4869a21f43ce7b00ec0c4c1ee9d50f0005
Sam Almeida <samRAlmeida@icloud.com>
2017-05-12 15:18:55 -0400

    contact modal working

---
+++ public/js/main.js
@@ -0,0 +1,61 @@
+
+var modal = document.getElementById("modal");
+var blur = document.getElementById("shadow");
+var close = document.getElementById("close");
+
+setTimeout(function(){
+	modal.style.display = 'inline-flex'; 
+	blur.classList.add("blur");
+}, 1000);
+
+	close.addEventListener("click", function(){
+	blur.classList.add("fadeoutBlur");
+	blur.classList.remove("blur");
+	modal.classList.add("fadeout");
+	
+	setTimeout(function(){
+		modal.style.display = 'none'; 
+	}, 1250);
+});
+
+window.addEventListener("keyup", listenKey);
+function listenKey(e) {
+	if (e.keyCode == "27") {
+		modal.classList.add("fadeout");
+		blur.classList.add("fadeoutBlur");
+		blur.classList.remove("blur");
+		
+		setTimeout(function(){
+			modal.style.display = 'none'; 
+	}, 1250);
+		
+	} else if (e.keyCode == "32") {
+		modal.classList.remove("fadeout")
+		blur.classList.remove("fadeoutBlur")
+		modal.style.display = 'inline-flex';
+		blur.classList.add("blur");
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
