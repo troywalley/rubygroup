4bbb50f7d111dd3985099cb9f0aa22d99b849b75
Troy Walley <walley.troy@yahoo.com>
2017-05-14 21:26:19 -0400

    gallery

--- public/js/main.js
+++ public/js/main.js
@@ -1,43 +1,3 @@
-
-// var modal = document.getElementById("modal");
-// var blur = document.getElementById("shadow");
-// var close = document.getElementById("close");
-
-// setTimeout(function(){
-// 	modal.style.display = 'inline-flex'; 
-// 	blur.classList.add("blur");
-// }, 500);
-
-// 	close.addEventListener("click", function(){
-// 	blur.classList.add("fadeoutBlur");
-// 	blur.classList.remove("blur");
-// 	modal.classList.add("fadeout");
-	
-// 	setTimeout(function(){
-// 		modal.style.display = 'none'; 
-// 	}, 1250);
-// });
-
-// window.addEventListener("keyup", listenKey);
-// function listenKey(e) {
-// 	if (e.keyCode == "27") {
-// 		modal.classList.add("fadeout");
-// 		blur.classList.add("fadeoutBlur");
-// 		blur.classList.remove("blur");
-		
-// 		setTimeout(function(){
-// 			modal.style.display = 'none'; 
-// 	}, 1250);
-		
-// 	} else if (e.keyCode == "32") {
-// 		modal.classList.remove("fadeout")
-// 		blur.classList.remove("fadeoutBlur")
-// 		modal.style.display = 'inline-flex';
-// 		blur.classList.add("blur");
-// 	}
-
-// }
-
 function dotSaver(){
 this.dot=[];
 this.menuList=[];
@@ -81,7 +41,96 @@ var dotsaver= new dotSaver()
 dotsaver.loop()
 dotsaver.addEL(dotsaver.menuList,dotsaver.dot)
 
-	console.log(dotsaver.menuList.length)
+	
+var carousel=document.getElementsByClassName("photo-container")[0]
+var arrowRight=document.getElementsByClassName("arrow-right")[0]
+var arrowLeft=document.getElementsByClassName("arrow-left")[0]
+arrowLeft.addEventListener("click", photoBackward)
+arrowRight.addEventListener("click", photoForward)
+function carousels(){
+	this.time=0;
+	this.picArray=["baclava.jpg","bougatsa.jpg","catering.jpg","finikia.jpg","latte.jpg","parfait.jpg","tyropitas.jpg"]
+	this.currentpic=1
+	this.start=startTime
+	var me=this;
+	function startTime(){
+		setTimeout(function(){
+			carousel.style.backgroundImage="url('"+me.picArray[me.currentpic]+"')"
+			me.time=0;
+			if(me.currentpic===me.picArray.length-1){
+				me.currentpic=0;
+			}else{
+		me.currentpic+=1;
+	}
+		startTime()
+		},4000)
+
+	}
+}
+var pics=new carousels();
+pics.start();
+
+function photoBackward(){
+pics.time=0;
+if(pics.currentpic===0){
+		pics.currentpic=pics.picArray.length-1;
+	}else{
+		pics.currentpic-=1;
+	}
+	carousel.style.backgroundImage="url('"+pics.picArray[pics.currentpic]+"')"
+}
+function photoForward(){
+pics.time=0;
+if(pics.currentpic===pics.picArray.length-1){
+		pics.currentpic=0;
+	}else{
+		pics.currentpic+=1;
+	}
+	carousel.style.backgroundImage="url('"+pics.picArray[pics.currentpic]+"')"
+}
+
+
+
+var modal = document.getElementById("modal");
+var blur = document.getElementById("shadow");
+var close = document.getElementById("close");
+
+setTimeout(function(){
+	modal.style.display = 'inline-flex'; 
+	blur.classList.add("blur");
+}, 500);
+
+	close.addEventListener("click", function(){
+	blur.classList.add("fadeoutBlur");
+	blur.classList.remove("blur");
+	modal.classList.add("fadeout");
+	
+	setTimeout(function(){
+		modal.style.display = 'none'; 
+	}, 1250);
+});
+
+window.addEventListener("keyup", listenKey);
+function listenKey(e) {
+	if (e.keyCode == "27") {
+		modal.classList.add("fadeout");
+		blur.classList.add("fadeoutBlur");
+		blur.classList.remove("blur");
+		
+		setTimeout(function(){
+			modal.style.display = 'none'; 
+	}, 1250);
+		
+	} else if (e.keyCode == "32") {
+		modal.classList.remove("fadeout")
+		blur.classList.remove("fadeoutBlur")
+		modal.style.display = 'inline-flex';
+		blur.classList.add("blur");
+	}
+
+}
+
+
 	
 
 
